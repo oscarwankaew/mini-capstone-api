dd821e80b63627ad6edf5c7589a1f6d8d2a355bf
Oscar Wankaew <oscar.wankaew@gmail.com>
2022-05-11 12:38:53 -0500

    adds validations to Product.rb

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,60 +1,65 @@
 class ProductsController < ApplicationController
-    def index
-        @products = Product.all
-        render template: "products/index"
-    end
-    
-    def show
-        @product = Product.find_by(id: params["id"])
-        render template: "products/show"
-    end
+  def index
+    @products = Product.all
+    render template: "products/index"
+  end
 
-    def create
-        product = Product.new(
-            name: params["name"],
-            price: params["price"],
-            image_url: params["image_url"],
-            description: params["description"]
-        )
-        product.save
-        @product = product
-        render template: "products/show"
+  def show
+    @product = Product.find_by(id: params["id"])
+    render template: "products/show"
+  end
+
+  def create
+    product = Product.new(
+      name: params["name"],
+      price: params["price"],
+      image_url: params["image_url"],
+      description: params["description"],
+    )
+    if product.save
+      @product = product
+      render template: "products/show"
+    else
+      render json: { errors: product.errors.full_messages }, status: :expectation_failed
     end
-    
-    def update
-        product_id = params["id"]
-        product = Product.find_by(id: product_id)
+  end
 
-        product.name = params["name"] || product.name
-        product.price = params["price"] || product.price
-        product.image_url = params["image_url"] || product.image_url
-        product.description = params["description"] || product.description
+  def update
+    product_id = params["id"]
+    product = Product.find_by(id: product_id)
 
-        product.save
-        @product = product
-        render template: "products/show"
+    product.name = params["name"] || product.name
+    product.price = params["price"] || product.price
+    product.image_url = params["image_url"] || product.image_url
+    product.description = params["description"] || product.description
+
+    if product.save
+      @product = product
+      render template: "products/show"
+    else
+      render json: { errors: product.errors.full_messages }, status: :expectation_failed
     end
+  end
 
-    def destroy
-        product_id = params["id"]
-        product = Product.find_by(id: product_id)
-        product.destroy
-        render json: {message: "Deleted the product"}
-    end
-
+  def destroy
+    product_id = params["id"]
+    product = Product.find_by(id: product_id)
+    product.destroy
+    render json: { message: "Deleted the product" }
+  end
 end
 
 # def bicycle_listing
-    #     listed_bicycle = Product.first
-    #     render json: listed_bicycle.as_json
-    # end
+#     listed_bicycle = Product.first
+#     render json: listed_bicycle.as_json
+# end
 
-    # def TV_listing
-    #     listed_TV = Product.second
-    #     render json: listed_TV.as_json
-    # end
+# def TV_listing
+#     listed_TV = Product.second
+#     render json: listed_TV.as_json
+# end
 
-    # def desk_listing
-    #     listed_desk = Product.last
-    #     render json: listed_desk.as_json
-    # end
+# def desk_listing
+#     listed_desk = Product.last
+#     render json: listed_desk.as_json
+# end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,20 +1,27 @@
 class Product < ApplicationRecord
-    def is_discounted?
-        price < 10
-    end
+  validates :name, presence: true
+  validates :price, presence: true
+  validates :price, numericality: { greater_than: 0 }
+  validates :description, presence: true
+  validates :description, length: { in: 10..500 }
 
-    def tax
-        tax = price * 0.09
-    end
+  def is_discounted?
+    price < 10
+  end
 
-    def total
-        total = price + tax
-    end
+  def tax
+    tax = price * 0.09
+  end
 
-    def friendly_created_at
-        return created_at.strftime("%e %b %Y %H:%M:%S%p")
-    end
-    def friendly_updated_at
-        return updated_at.strftime("%e %b %Y %H:%M:%S%p")
-    end
+  def total
+    total = price + tax
+  end
+
+  def friendly_created_at
+    return created_at.strftime("%e %b %Y %H:%M:%S%p")
+  end
+
+  def friendly_updated_at
+    return updated_at.strftime("%e %b %Y %H:%M:%S%p")
+  end
 end
