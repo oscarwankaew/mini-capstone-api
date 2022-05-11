d2398145c02db05cf0d945bacce930cac109dc7b
Oscar Wankaew <oscar.wankaew@gmail.com>
2022-05-11 15:57:36 -0500

    adds Supplier model to mini capstone app

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,3 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+Supplier.create(name: "Walmart", email: "walmart@test.com", phone_number: "222-333-444")
+Supplier.create(name: "Exclusive company", email: "exclusiveco@test.com", phone_number: "343-999-7777")
+Supplier.create(name: "Boring company", email: "boring.musk@test.com", phone_number: "123-456-7890")
