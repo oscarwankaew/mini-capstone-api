27dae701e9fad2f7080ca99431a7262379182a13
Oscar Wankaew <oscar.wankaew@gmail.com>
2022-05-20 12:56:34 -0500

    Adds destroy action to CartedProduct model to do remove action

--- app/controllers/carted_products_controller.rb
+++ app/controllers/carted_products_controller.rb
@@ -1,6 +1,6 @@
 class CartedProductsController < ApplicationController
   def create
-    carted_products = CartedProduct.new(
+    carted_product = CartedProduct.new(
       user_id: current_user.id,
       product_id: params[:product_id],
       quantity: params[:quantity],
@@ -14,5 +14,12 @@ class CartedProductsController < ApplicationController
   def index
     carted_products = current_user.carted_products.find_by(status: "carted")
     render json: carted_products.as_json
+  end
+
+  def destroy
+    carted_product = current_user.carted_products.find_by(id: params[:id], status: "carted")
+    carted_product.status = "removed"
+    carted_product.save
+    render json: { massege: "Selected item has been removed from your cart." }
   end
 end
